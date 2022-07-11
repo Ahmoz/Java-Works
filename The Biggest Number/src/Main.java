d7073a2dfa4fd8f83729ebd70ece2323c8b96bf6
Ahmoz <77580454+Ahmoz@users.noreply.github.com>
2022-07-11 03:25:22 +0300

    Add files via upload

---
+++ The Biggest Number/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x,y,z;
+        x = 5;
+        y = 3;
+        z = 4;
+
+        if(x>y&&x>z){
+            System.out.println("the biggest number is x -> "+x);
+        }
+        else if(y>x&&y>z){
+            System.out.println("the biggest number is y ->"+y);
+        }
+        else{
+            System.out.println("The biggest number is z -> " + z);
+        }
+
+
+
+         }
+}
